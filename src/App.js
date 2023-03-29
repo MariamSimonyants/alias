50cdd3890d4d84033910ef5cb238559beafab9eb
Volo Trainer <trainer@volo.global>
2023-03-29 16:06:53 +0400

    header done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './comonents/Header/Header';
+import { TextField } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      Alias by Shirakatsy
+      < Header />
+      <TextField id="outlined-basic" label="Team 1 Name" variant="outlined" />
+      <TextField id="outlined-basic" label="Team 2 Name" variant="outlined" />
     </div>
   );
 }
